77ce41b7afca910dc05ac1ca7aa1bda43e6a70cd
NickSipes <nicktsipes@gmail.com>
2020-04-18 13:21:47 -0400

    Dynamic Model of fixed inverted pendelum

---
+++ Inv_Pend_Fixed.m
@@ -0,0 +1,62 @@
+%% Inverted Pendulum
+% RBE502 Group Project
+% % % % % % % % % % % % % % % % %
+clc
+close all
+clear all
+
+%% Kinetic and Potential Energies
+% q1 -> body angle
+% q2 -> wheel angle
+syms m_wheel l_body q1 q2 dq1 dq2 ddq1 ddq2 g r1 r2
+
+I_wheel_rotation = 0.5 * m_wheel * (r1^2 + r2^2);
+I_body = m_wheel * l_body^2;
+
+U = m_wheel * g * l_body * sind(q1);
+T = 0.5 * I_wheel_rotation * dq2^2 + 0.5*I_body*dq1^2;
+
+%% Lagrange Equation
+L = simplify(T-U);
+
+%% Derive the dynamics
+% Solve for TorqueDynam
+T1 = simplify(diff(L,dq1)*ddq1 - diff(L,q1)*dq1);
+T2 = simplify(diff(L,dq2)*ddq2 - diff(L,q2)*dq2);
+Tau = [T1;T2];
+
+% Solve for M
+M11 = simplify(expand(T1 - subs(T1,ddq1,0))/ddq1);
+M12 = simplify(expand(T1 - subs(T1,ddq2,0))/ddq2);
+M21 = simplify(expand(T2 - subs(T2,ddq1,0))/ddq1);
+M22 = simplify(expand(T2 - subs(T2,ddq2,0))/ddq2);
+M = [M11 M12;
+M21 M22];
+
+% solve for G
+G1 = subs(T1,{ddq1,ddq2,dq1,dq2},{0,0,0,0});
+G2 = subs(T2,{ddq1,ddq2,dq1,dq2},{0,0,0,0});
+G = [G1;G2];
+
+% solve for C
+C1 = simplify(expand(T1 - M(1,:)*[ddq1 ddq2].' + G(1)));
+C2 = simplify(expand(T2 - M(2,:)*[ddq1 ddq2].' + G(2)));
+C = [C1;C2];
+
+%     subs(M,[mt,dq1,dq2,dq3,g],[.5,q1_d,q2_d,q3_d,g]);
+%     subs(C,[mt,dq3,g],[mt,q3_d,g]);
+
+% state-space format.
+syms u1 u2
+
+% 2 DOF robot = 4 states. 
+SS = M \ (simplify([u1;u2] - C));
+dx = [dq1;dq2;SS(1);SS(2)];
+
+function [Transform] = DH(theta,d,a,alpha)
+
+Transform = [cos(theta) -sin(theta)*cos(alpha) sin(theta)*sin(alpha) a*cos(theta);
+    sin(theta) cos(theta)*cos(alpha) -cos(theta)*sin(alpha) a*sin(theta);
+    0 sin(alpha) cos(alpha) d;
+    0 0 0 1];
+end
